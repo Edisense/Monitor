dedc487b93b51d99e86af56bd573648d7264b72d
jshack02 <shackjb@gmail.com>
2014-12-11 18:19:25 -0800

    Cleaned up prints and started daemon right from main

--- main.cpp
+++ main.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
+#include <chrono>
 #include <QtWidgets/qapplication.h>
 #include <HomeController.h>
+#include "PollerDaemon.h"
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
+
+  std::cout << "Monitor Test ******* " << std::endl;
+  PollerDaemon poll_d;
+  std::list<device_t> devices;
+  int dev_id_start = 1;
+  int dev_id_max = 50;
+  for (int i = dev_id_start; i < dev_id_max; ++i)
+    devices.push_back(i);
+  poll_d.start(devices, "corn34.stanford.edu");
+  volatile  int i = 0;
+  while(1)
+    {
+      ++i;
+      std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+
   QApplication app(argc, argv);
   HomeController* appController = new HomeController;
   appController->start();
--- src/model/PollerDaemon.cpp
+++ src/model/PollerDaemon.cpp
@@ -9,6 +9,8 @@ void PollerDaemon::start(std::list<device_t> devices, std::string startHost) {
   client = new edisense_comms::Client();
   this->devices = devices;
   this->knownHost = startHost; //We have to have at least one known host to query the master list
+  relocate();
+  run();
 }
 
 void PollerDaemon::stop() {
@@ -16,15 +18,15 @@ void PollerDaemon::stop() {
 }
 
 void PollerDaemon::relocate() {
-  std::cout << "Relocating!!" << std::endl;
   std::map<device_t, std::list<std::string>> hostsByDev;
   devsByHost.clear(); //reset the master list
   for(std::list<device_t>::iterator it = devices.begin(); it != devices.end(); ++it) {
-
     std::future<std::list<std::string>> hostIDs = client->locate(*it, knownHost);
-
     std::future_status status = hostIDs.wait_for(std::chrono::seconds(2));
-    //ASSERT(std::future_status::ready == status);
+
+    std::cout << "Give me all hosts that store device: " << *it << ". Asking: " << knownHost << std::endl;
+
+    assert(std::future_status::ready == status);
     std::list<std::string> results = hostIDs.get();
     //Any reason to not always use knownHost?
     hostsByDev.insert(std::pair<device_t, std::list<std::string>>(*it, results));
@@ -55,13 +57,15 @@ void PollerDaemon::run() {
   bool relocate_f = false;
   std::future<std::list<GetResult>> resultData;
   while (shouldRun) {
-
     time(&endT);
     startT = endT - QUERY_WINDOW;
 
     for (std::map<std::string, std::list<device_t>>::iterator it = devsByHost.begin(); it != devsByHost.end(); ++it) {
       std::list<std::string> tempHost;
       tempHost.push_back(it->first);
+
+      std::cout << std::endl;
+
       for (std::list<device_t>::iterator iit = it->second.begin(); iit != it->second.end(); iit++) {
         resultData = client->get(++tid, tempHost, *iit, startT, endT);
 	std::future_status status = resultData.wait_for(std::chrono::seconds(2));
@@ -72,7 +76,8 @@ void PollerDaemon::run() {
 	if (results.begin()->status == SUCCESS) {
           std::list<Data> readings = *results.begin()->values;
           for (std::list<Data>::iterator j = readings.begin(); j != readings.end(); j++) {
-            std::cout << "host: " << it->first << " | device: " << *iit << " | data: " << j->data << std::endl;
+	    long long* temp_data = reinterpret_cast<long long*>(&j->data[0]);
+            std::cout << "host: " << it->first << " | device: " << *iit << " | data: " << *temp_data << std::endl;
 	  }
 	}
         else {
